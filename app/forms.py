e986405b42a057e0351d7eb8969abb4cb125c8b4
Rohan.vs <rsuri@irusmail.com>
2020-05-15 22:10:27 -0400

    more tings

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     link = StringField("Link!!", validators=[DataRequired()])
     submit = SubmitField("Submit", validators=[DataRequired()])
+    metaa = BooleanField("MetaData?", validators=[DataRequired()])
