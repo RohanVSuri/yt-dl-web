cf3921b7dadc5c2e572fd987edd063a7e722a4b8
Rohan.vs <rsuri@irusmail.com>
2020-07-11 20:32:22 -0400

    taglib is hard to install on heroku

--- app/download.py
+++ app/download.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 import os
 from os import listdir
-import taglib
+# import taglib
 class Down():
     def __init__(self, link, itag, title = "YouTube"):
         self.link = link
@@ -26,12 +26,12 @@ class Down():
         clip = mp.VideoFileClip(f'app/tmp/{self.title}.mp4')
         clip.audio.write_audiofile(f'app/tmp/{self.title}.mp3')
         clip.close()
-    def change_metadata(self, title, artist, album):
-        song = taglib.File(f"{self.path}{self.title}.mp4")
-        song.tags["ARTIST"] = artist
-        song.tags["ALBUM"] = album
-        song.tags["TITLE"] = title
-        song.save()
+    # def change_metadata(self, title, artist, album):
+    #     song = taglib.File(f"{self.path}{self.title}.mp4") - installing taglib is hard :(
+    #     song.tags["ARTIST"] = artist
+    #     song.tags["ALBUM"] = album
+    #     song.tags["TITLE"] = title
+    #     song.save()
 
     def clear_folder(self):
         for file_name in listdir(self.path):
